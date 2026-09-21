cdfe174d631f9dc40abf219ed587d57cce361524
Jake Sedgwick <jakedotsed@gmail.com>
2026-09-21 11:38:42 +1000

    Made player change animations to reflect direction

--- Assets/Scripts/PlayerMover.cs
+++ Assets/Scripts/PlayerMover.cs
@@ -4,10 +4,14 @@ public class PlayerMover : MonoBehaviour
 {
     [SerializeField]
     private GameObject player;
+    Animator animator;
     private Tweener tweener;
+    private int direction;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
+        animator = player.GetComponent<Animator>();
+
         tweener = GetComponent<Tweener>();
         tweener.AddTween(player.transform, player.transform.position, player.transform.position + new Vector3(5f, 0, 0), 0f, 2.5f);
         tweener.AddTween(player.transform, player.transform.position + new Vector3(5f, 0, 0), player.transform.position + new Vector3(5f, -4f, 0), 2.5f, 2f);
@@ -18,6 +22,26 @@ public class PlayerMover : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if(Time.time < 2.5f && direction != 3)
+        {
+            animator.SetInteger("Direction", 3);
+            direction = 3;
+        }
+        else if(Time.time >= 2.5f && Time.time < 4.5f && direction != 0)
+        {
+            animator.SetInteger("Direction", 0);
+            direction = 0;
+        }
+        else if(Time.time >= 4.5f && Time.time < 7f && direction != 1)
+        {
+            animator.SetInteger("Direction", 1);
+            direction = 1;
+        }
+        else if(Time.time >= 7f && direction != 2)
+        {
+            animator.SetInteger("Direction", 2);
+            direction = 2;
+        }
         
     }
 }
